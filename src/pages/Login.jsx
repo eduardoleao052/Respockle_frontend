873a5584df52341eeae238aa8db0e04052e013b8
Terciotti <pedro.terciotti@usp.br>
2024-12-02 01:15:09 -0300

    Change Browser to Hash

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,17 @@
 import Form from '../components/Form'
 import '../styles/Login.css'
-import {useLocation} from  'react-router-dom'
+import {useLocation,useNavigate} from  'react-router-dom'
 
 export default function Login() {
   const location = useLocation();
-  const from = location.state?.from?.pathname || '/'; 
+  const from = location.state?.from?.pathname || '/';
+  const navigateTo = useNavigate(); 
 
   return (
     <div style={{textAlign: "center", width: "100%", marginTop: "140px"}}>
       <Form url={from} route="/api/token/" method="login" />
       <p>Don't have an account?</p>
-      <a href="/register"><button className='register-button'>Register</button></a>
+      <button className='register-button' onClick={() => navigateTo('/register')}>Register</button>
     </div>
   )
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import Form from '../components/Form'
 import "../styles/Login.css"
-import {useLocation} from  'react-router-dom'
+import {useLocation,useNavigate} from  'react-router-dom'
 
 
 export default function Register() {
   const location = useLocation();
-  const from = location.state?.from?.pathname || '/'; 
+  const from = location.state?.from?.pathname || '/';
+  const navigateTo = useNavigate();  
 
   return (
     <div style={{textAlign: "center", width: "100%", marginTop: "140px"}}>
       <Form url={from} route="/api/user/register/" method="register" />
       <p>Already have an account?</p>
-      <a href="/login"><button className='register-button'>Login</button></a>
+      <button className='register-button' onClick={() => navigateTo('/login')}>Login</button>
     </div>
   )
 }
